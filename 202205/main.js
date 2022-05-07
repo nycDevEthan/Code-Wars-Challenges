3d86aa59cb09231d5d826d73af21dd8e177d01a0
ejolikestocode <90877524+ejolikestocode@users.noreply.github.com>
2022-05-06 23:24:57 -0400

    Added 5/6/22 coding solution

--- 202205/main.js
+++ 202205/main.js
@@ -109,3 +109,14 @@ function findNeedle(haystack) {
   
   return `found the ${needleInHayStack} at position ${haystack.indexOf('needle')}`;
 }
+
+// Ethan O'Connell - 5/6/22
+// 8 kyu You only need one - Beginner
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+// Array can contain numbers or strings. X can be either.
+// Return true if the array contains the value, false if not.
+
+// Solution
+function check(a, x) {
+  return a.includes(x);
+}
