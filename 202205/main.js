406e27ce7081b2c7c06cc073318622f25a47b45a
ejolikestocode <90877524+ejolikestocode@users.noreply.github.com>
2022-05-08 12:37:12 -0400

    Added 5/8/22 coding solution

--- 202205/main.js
+++ 202205/main.js
@@ -133,3 +133,15 @@ function solution(str){
   strArr = strArr.reverse();
   return strArr.join('');
 }
+
+// Ethan O'Connell - 5/8/22
+// 8 kyu Convert a Number to a String!
+// We need a function that can transform a number into a string.
+// What ways of achieving this do you know?
+// Examples:
+// 123 --> "123"
+// 999 --> "999"
+
+function numberToString(num) {
+  return num.toString();
+}
