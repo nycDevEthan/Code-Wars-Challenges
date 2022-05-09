ba1fc712a20ee856b9cd4db51562c5e7ed69c4c0
ejolikestocode <90877524+ejolikestocode@users.noreply.github.com>
2022-05-09 12:38:36 -0400

    Added 5/9/22 coding solution

--- 202205/main.js
+++ 202205/main.js
@@ -142,6 +142,16 @@ function solution(str){
 // 123 --> "123"
 // 999 --> "999"
 
+// Solution
 function numberToString(num) {
   return num.toString();
+}
+
+// Ethan O'Connell - 5/8/22
+// 8 kyu You Can't Code Under Pressure #1
+// Code as fast as you can! You need to double the integer and return it.
+
+// Solution
+function doubleInteger(i) {
+  return i * 2;
 }
