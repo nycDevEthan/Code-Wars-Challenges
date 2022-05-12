fb0d3c076058f53740caa2f8d46ef0fc673d8dcf
ejolikestocode <90877524+ejolikestocode@users.noreply.github.com>
2022-05-11 20:04:40 -0400

    Added 05/11/22 coding solution

--- 202205/main.js
+++ 202205/main.js
@@ -167,3 +167,19 @@ function doubleInteger(i) {
 
 // Solution
 const greet = greeting => 'hello world!';
+
+// Ethan O'Connell - 5/11/22
+// 8 kyu L1: Set Alarm
+// Write a function named setAlarm which receives two parameters. The first parameter, employed, is true whenever you are employed and the second parameter, vacation is true whenever you are on vacation.
+
+// The function should return true if you are employed and not on vacation (because these are the circumstances under which you need to set an alarm). It should return false otherwise. Examples:
+
+// setAlarm(true, true) -> false
+// setAlarm(false, true) -> false
+// setAlarm(false, false) -> false
+// setAlarm(true, false) -> true
+
+// Solution
+function setAlarm(employed, vacation){
+  return true ? employed === true && vacation === false : false;
+}
