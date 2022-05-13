bde06a0abb1f74f51e93a35da6b02f13c1965cb2
ejolikestocode <90877524+ejolikestocode@users.noreply.github.com>
2022-05-12 22:22:18 -0400

    Added 05/12/22 coding solution

--- 202205/main.js
+++ 202205/main.js
@@ -183,3 +183,22 @@ const greet = greeting => 'hello world!';
 function setAlarm(employed, vacation){
   return true ? employed === true && vacation === false : false;
 }
+
+// Ethan O'Connell - 5/12/22
+// 8 kyu Sum of positive
+// You get an array of numbers, return the sum of all of the positives ones.
+
+// Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+// Note: if there is nothing to sum, the sum is default to 0.
+
+// Solution
+function positiveSum(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (Math.sign(arr[i]) === 1) {
+      sum += arr[i];
+    }
+  }
+  return sum;
+}
