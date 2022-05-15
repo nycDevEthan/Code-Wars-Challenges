3a20ebdbf44f29753a19a06b90a878b8155acf56
ejolikestocode <90877524+ejolikestocode@users.noreply.github.com>
2022-05-14 21:11:58 -0400

    Added 05/14/22 coding solution

--- 202205/main.js
+++ 202205/main.js
@@ -231,3 +231,12 @@ function countPositivesSumNegatives(input) {
   posCountAndNegSum.push(negSum);
   return posCountAndNegSum;
 }
+
+// Ethan O'Connell - 5/14/22
+// 8 kyu Convert boolean values to strings 'Yes' or 'No'.
+// Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+
+function boolToWord( bool ){
+  if (bool === true) return "Yes";
+  else return "No";
+}
