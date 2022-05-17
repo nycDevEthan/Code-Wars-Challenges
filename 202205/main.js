7a2f2aafa87e6a893b47920645a2d8d483577d83
ejolikestocode <90877524+ejolikestocode@users.noreply.github.com>
2022-05-16 22:09:59 -0400

    Added 05/16/22 coding solution

--- 202205/main.js
+++ 202205/main.js
@@ -247,3 +247,32 @@ function boolToWord( bool ){
 function even_or_odd(number) {
   return number % 2 === 0 ? 'Even' : 'Odd';
 }
+
+// Ethan O'Connell - 5/16/22
+// 8 kyu Convert a String to a Number!
+// Note: This kata is inspired by Convert a Number to a String!. Try that one too.
+
+// Description
+// We need a function that can transform a string into a number. What ways of achieving this do you know?
+
+// Note: Don't worry, all inputs will be strings, and every string is a perfectly valid representation of an integral number.
+
+const stringToNumber = function(str){
+  return Number(str);
+}
+
+// Ethan O'Connell - 5/16/22
+// 7 kyu Regex validate PIN code
+// ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+
+// If the function is passed a valid PIN string, return true, else return false.
+
+// Examples (Input --> Output)
+// "1234"   -->  true
+// "12345"  -->  false
+// "a234"   -->  false
+
+function validatePIN (pin) {
+  if (Number(pin) >= 0 && Number(pin) % 1 === 0 && (pin.length === 4 || pin.length === 6)) return true;
+  else return false;
+}
