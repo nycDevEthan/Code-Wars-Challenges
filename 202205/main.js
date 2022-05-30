86e9e110d6e0ccce5e07a23dc9fdf0c603bd2eda
ejolikestocode <90877524+ejolikestocode@users.noreply.github.com>
2022-05-29 23:50:44 -0400

    Added 05/29/22 coding solution

--- 202205/main.js
+++ 202205/main.js
@@ -377,3 +377,22 @@ function noSpace(x){
 function removeChar(str){
   return str.slice(1, -1);
 };
+
+// Ethan O'Connell - 5/29/22
+// 8 kyu
+// Find the smallest integer in the array
+// Given an array of integers your solution should find the smallest integer.
+
+// For example:
+
+// Given [34, 15, 88, 2] your solution will return 2
+// Given [34, -345, -1, 100] your solution will return -345
+// You can assume, for the purpose of this kata, that the supplied array will not be empty.
+
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    args.sort((a,b) => a-b);
+    return args[0];
+  }
+}
+
