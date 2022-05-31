db5bb188fa6657555a685fedd89a12028870613a
ejolikestocode <90877524+ejolikestocode@users.noreply.github.com>
2022-05-31 10:31:16 -0400

    Added 5/31/22 coding solution

--- 202205/main.js
+++ 202205/main.js
@@ -417,3 +417,30 @@ var summation = function (num) {
   }
   return sum;
 }
+
+// Ethan O'Connell - 5/31/22
+// 8 kyu
+// Counting sheep...
+// Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+// For example,
+
+// [true,  true,  true,  false,
+//   true,  true,  true,  true ,
+//   true,  false, true,  false,
+//   true,  false, false, true ,
+//   true,  true,  true,  true ,
+//   false, false, true,  true]
+// The correct answer would be 17.
+
+// Hint: Don't forget to check for bad values like null/undefined
+
+function countSheeps(arrayOfSheep) {
+  let numOfSheep = 0;
+  for (let i = 0; i < arrayOfSheep.length; i++) {
+    if (arrayOfSheep[i] === true) {
+      numOfSheep++;
+    }
+  }
+  return numOfSheep;
+}
