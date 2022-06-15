94ba1ae40992df306dd4b390e40c1c3bb2567202
ejolikestocode <90877524+ejolikestocode@users.noreply.github.com>
2022-06-15 12:40:10 -0400

    Add 6/15/22 coding solution

--- 202206/main.js
+++ 202206/main.js
@@ -169,3 +169,33 @@ function bmi(weight, height) {
   else if (bmi <= 30.0) return "Overweight"
   else return "Obese";
 }
+
+// Ethan O'Connell - 6/15/22
+// 8 kyu
+// Sum without highest and lowest number
+// DESCRIPTION:
+// Task
+// Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+
+// The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+
+// Mind the input validation.
+
+// Example
+// { 6, 2, 1, 8, 10 } => 16
+// { 1, 1, 11, 2, 3 } => 6
+// Input validation
+// If an empty value ( null, None, Nothing etc. ) is given instead of an array, or the given array is an empty list or a list with only 1 element, return 0.
+
+function sumArray(array) {
+  let sum = 0;
+  if (array === null || array === undefined || array === {}) {
+    return 0;
+  } else {
+    array.sort((a,b) => a - b);
+    for (let i = 1; i < array.length - 1; i++) {
+      sum += array[i];
+    }
+    return sum;
+  }
+}
