79a13abd645b66c92347ec99306d7c240fcc9b3e
ejolikestocode <90877524+ejolikestocode@users.noreply.github.com>
2022-06-14 21:22:21 -0400

    Added 6/14/22 coding solution

--- 202206/main.js
+++ 202206/main.js
@@ -148,3 +148,24 @@ function getGrade (s1, s2, s3) {
     return 'F';
   }
 }
+
+// Ethan O'Connell - 6/14/22
+// 8 kyu
+// Calculate BMI
+// Write function bmi that calculates body mass index (bmi = weight / height2).
+
+// if bmi <= 18.5 return "Underweight"
+
+// if bmi <= 25.0 return "Normal"
+
+// if bmi <= 30.0 return "Overweight"
+
+// if bmi > 30 return "Obese"
+
+function bmi(weight, height) {
+  let bmi = weight / (height * height);
+  if (bmi <= 18.5) return "Underweight"
+  else if (bmi <= 25.0) return "Normal"
+  else if (bmi <= 30.0) return "Overweight"
+  else return "Obese";
+}
