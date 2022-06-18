4ed90c6abe6f29949cb24ef160446ce62bff771e
ejolikestocode <90877524+ejolikestocode@users.noreply.github.com>
2022-06-17 23:02:05 -0400

    Added 6/17/22 coding solution

--- 202206/main.js
+++ 202206/main.js
@@ -217,7 +217,13 @@ function feast(beast, dish) {
     }
 }
 
-// top solution - simplified version of mine
-function feast(beast, dish) {
-  return beast[0] === dish[0] && beast[beast.length - 1] === dish[dish.length - 1];
+// Ethan O'Connell - 6/17/22
+// 8 kyu
+// Opposites Attract
+// Timmy & Sarah think they are in love, but around where they live, they will only know once they pick a flower each. If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+
+// Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't.
+
+function lovefunc(flower1, flower2){
+  return true ? flower1 % 2 === 0 && flower2 % 2 !== 0 || flower1 % 2 !== 0 && flower2 % 2 === 0 : false;
 }
