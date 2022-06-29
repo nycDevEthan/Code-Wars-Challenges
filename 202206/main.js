87f104252b23c5c549be66dcf14024dd77e8303f
ejolikestocode <ethanoconnell456@gmail.com>
2022-06-29 19:16:07 -0400

    Add 6/29/22 code solution

--- 202206/main.js
+++ 202206/main.js
@@ -435,3 +435,23 @@ function paperwork(n, m) {
   }
   return n * m;
 }
+
+// Ethan O'Connell - 6/29/22
+// 8 kyu
+// Fake Binary
+// Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+// Note: input will never be an empty string
+
+function fakeBin(x){
+  x = x.split('');
+  console.log(x);
+  for (let i = 0; i < x.length; i++) {
+    if (x[i] < 5) {
+      x[i] = 0;
+    } else {
+      x[i] = 1;
+    }
+  }
+  return x.join('');
+}
