0a73e625da816cefc51ed6db2d11c3808e4f73c9
ejolikestocode <ethanoconnell456@gmail.com>
2022-07-13 15:42:09 -0400

    Add 7/13/22 code solution

--- 202207/main.js
+++ 202207/main.js
@@ -202,3 +202,21 @@ class Kata {
       return length * width * height;
     }
 }
+
+// Ethan O'Connell - 7/12/22
+// 8 kyu
+// If you can't sleep, just count sheep!!
+// If you can't sleep, just count sheep!!
+
+// Task:
+// Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers.
+
+var countSheep = function (num){
+    let str = '';
+    
+    for (let i = 1; i <= num; i++) {
+        str += `${i} sheep...`; 
+    }
+    
+    return str;
+}
