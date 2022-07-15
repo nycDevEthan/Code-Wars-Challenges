8c000862f4b35b5239123528792ddf2576e0ff6f
ejolikestocode <ethanoconnell456@gmail.com>
2022-07-15 16:14:17 -0400

    Add 7/15/22 code solution

--- 202207/main.js
+++ 202207/main.js
@@ -242,3 +242,24 @@ function arrayPlusArray(arr1, arr2) {
 function arrayPlusArray(arr1, arr2) {
     return arr1.concat(arr2).reduce((previousValue, currentValue) => previousValue + currentValue);
 }
+
+// Ethan O'Connell - 7/14/22
+// 8 kyu
+// Can we divide it?
+// Your task is to create the functionisDivideBy (or is_divide_by) to check if an integer number is divisible by both integers a and b.
+
+// A few cases:
+
+
+// (-12, 2, -6)  ->  true
+// (-12, 2, -5)  ->  false
+
+// (45, 1, 6)    ->  false
+// (45, 5, 15)   ->  true
+
+// (4, 1, 4)     ->  true
+// (15, -5, 3)   ->  true
+
+function isDivideBy(number, a, b) {
+    return true ? (number % a === 0) &&  (number % b === 0) : false;
+}
