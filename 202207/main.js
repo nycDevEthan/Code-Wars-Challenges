2d80bd7bbaae86c421891a22ced46e62310a56e8
ejolikestocode <ethanoconnell456@gmail.com>
2022-07-18 22:25:06 -0400

    Add 7/18/22 code solution

--- 202207/main.js
+++ 202207/main.js
@@ -286,3 +286,23 @@ function greet (name, owner) {
 function greet (name, owner) {
     return (name === owner) ? 'Hello boss' : `Hello guest`;
 }
+
+// Ethan O'Connell - 7/18/22
+// Rock Paper Scissors
+// Let's play! You have to return which player won! In case of a draw return Draw!.
+
+// Examples(Input1, Input2 --> Output):
+
+// "scissors", "paper" --> "Player 1 won!"
+// "scissors", "rock" --> "Player 2 won!"
+// "paper", "paper" --> "Draw!"
+
+const rps = (p1, p2) => {
+    if (p1 === p2) {
+      return 'Draw!';
+    } else if (p1 === 'scissors' && p2 === 'paper' || p1 === 'paper' && p2 === 'rock' || p1 === 'rock' && p2 === 'scissors') {
+      return 'Player 1 won!';
+    } else {
+      return 'Player 2 won!';
+    }
+};
