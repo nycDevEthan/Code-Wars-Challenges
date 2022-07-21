f232c33f6d6f624b16a3d74b090d9dcb5b759acc
ejolikestocode <ethanoconnell456@gmail.com>
2022-07-21 18:02:59 -0400

    Add 7/21/22 code solution

--- 202207/main.js
+++ 202207/main.js
@@ -307,7 +307,7 @@ const rps = (p1, p2) => {
     }
 };
 
-// Ethan O'Connell - 7/18/22
+// Ethan O'Connell - 7/19/22
 // 8 kyu
 // Count the Monkeys!
 // You take your son to the forest to see the monkeys. You know that there are a certain number there (n), but your son is too young to just appreciate the full number, he has to start counting them from 1.
@@ -357,4 +357,45 @@ function points(games) {
       }
     }
     return sum;
+}
+
+// Ethan O'Connell - 7/21/22
+// 8 kyu
+// Count by X
+// DESCRIPTION:
+// Create a function with two arguments that will return an array of the first (n) multiples of (x).
+
+// Assume both the given number and the number of times to count will be positive numbers greater than 0.
+
+// Return the results as an array (or list in Python, Haskell or Elixir).
+
+// Examples:
+
+// countBy(1,10) === [1,2,3,4,5,6,7,8,9,10]
+// countBy(2,5) === [2,4,6,8,10]
+
+// original solution
+
+function countBy(x, n) {
+    let z = [];
+    let y = x;
+    
+    for(let i=0;i<n;i++){
+      z.push(y);
+      y += x;
+    }
+  
+    return z;
+}
+
+// Optimal solution, realized I could just multiply x by increment variable after reviewing code solutions.  Much better!
+
+function countBy(x, n) {
+    let z = [];
+    
+    for (let i = 1; i <= n; i++) {
+      z.push(x * i);
+    }
+    
+    return z;
 }
